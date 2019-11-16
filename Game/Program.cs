663281262bd072434c657c79d37f49ebe010a8d6
Percy Qaz <percyqaz@yahoo.co.uk>
2019-11-16 12:57:41 +0000

    basic text rendering abilities with console - needs optimisation - needs color support

--- Game/Program.cs
+++ Game/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Game
 {
@@ -10,7 +11,14 @@ namespace Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            var s = new Screen();
+            int i = 0;
+            while (true)
+            {
+                s.WriteText(i+=1, 0, "Hello world!", Color.White, Color.Black);
+                s.Redraw();
+            Console.ReadLine();
+            }
         }
     }
 }
---
+++ Game/Screen.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Game
+{
+    class Screen
+    {
+        public static readonly int WIDTH = 80;
+        public static readonly int HEIGHT = 30;
+
+        struct Pixel
+        {
+            public char Char;
+            public Color Fore;
+            public Color Back;
+            public int Checksum;
+
+            public Pixel(char c, Color f, Color b)
+            {
+                Char = c;
+                Fore = f;
+                Back = b;
+                Checksum = c + f.ToArgb() * 7 + b.ToArgb() * 11;
+            }
+
+            public override string ToString()
+            {
+                //color code goes here
+                return Char.ToString();
+            }
+        }
+
+        Pixel[,] Pixels;
+        Pixel[,] OldPixels;
+
+        public Screen()
+        {
+            Console.SetWindowSize(WIDTH, HEIGHT);
+            Pixels = new Pixel[WIDTH, HEIGHT];
+            OldPixels = new Pixel[WIDTH, HEIGHT];
+            for (int x = 0; x < WIDTH; x++)
+            {
+                for (int y = 0; y < HEIGHT; y++)
+                {
+                    Pixels[x, y] = new Pixel(' ', Color.White, Color.Black);
+                    OldPixels[x, y] = new Pixel(' ', Color.White, Color.Black);
+                }
+            }
+        }
+
+        public void Redraw()
+        {
+            for (int y = 0; y < HEIGHT; y++)
+            {
+                for (int x = 0; x < WIDTH; x++)
+                {
+                    if (Pixels[x, y].Checksum != OldPixels[x, y].Checksum)
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(Pixels[x, y].ToString());
+                    }
+                }
+            }
+        }
+
+        public void WritePixel(int x, int y, char c, Color f, Color b)
+        {
+            if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) return;
+            OldPixels[x, y] = Pixels[x, y];
+            Pixels[x, y] = new Pixel(c, f, b);
+        }
+
+        public void WriteText(int x, int y, string text, Color f, Color b)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                WritePixel(x + i, y, text[i], f, b);
+            }
+        }
+    }
+}
